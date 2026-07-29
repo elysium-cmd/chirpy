7c5e5be7daca9d456af6f6975c98fe25fce0241a
elysium-cmd <eahodge1@proton.me>
2026-07-29 01:16:53 -0700

    Created basic local http server

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
