4721a2542b64903a871930de4e81d5db8a4e9699
elysium-cmd <eahodge1@proton.me>
2026-07-29 02:12:06 -0700

    Added metric middleware

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"sync/atomic"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Store(cfg.fileServerHits.Add(1))
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	apiCfg := apiConfig{}
+	apiCfg.fileServerHits.Store(0)
+	mux.Handle("/app/", apiCfg.middlewareMetrics(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
-		})
+	})
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.fileServerHits.Load())))
+	})
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileServerHits.Store(0)
+		w.WriteHeader(200)
+	})
 	server := http.Server{
 		Addr: ":8080",
 		Handler: mux,
