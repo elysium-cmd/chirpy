9820921a0268e37b7ff945b4b3d5eca1da80a81d
elysium-cmd <eahodge1@proton.me>
2026-08-14 17:42:07 -0700

    Rearranged functions

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ type apiConfig struct {
 	fileServerHits atomic.Int32
 }
 
+type errors struct {
+	Error string `json:"error"`
+}
+
+type valid struct {
+	Valid bool `json:"valid"`
+}
+
 func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileServerHits.Store(cfg.fileServerHits.Add(1))
@@ -19,71 +27,73 @@ func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
 	})
 }
 
+func validate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	if len(params.Body) > 140 {
+		respBody := errors{
+			Error: "Chirp is too long",
+		}
+		data, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(data)
+	} else {
+		respBody := valid{
+			Valid: true,
+		}
+		data, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(data)
+	}
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func metrics(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", apiCfg.fileServerHits.Load())))
+}
+
+func reset(w http.ResponseWriter, r *http.Request) {
+	apiCfg.fileServerHits.Store(0)
+	w.WriteHeader(200)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	apiCfg := apiConfig{}
 	apiCfg.fileServerHits.Store(0)
 	mux.Handle("/app/", apiCfg.middlewareMetrics(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
-		type parameters struct {
-			Body string `json:"body"`
-		}
-		type errors struct {
-			Error string `json:"error"`
-		}
-		type valid struct {
-			Valid bool `json:"valid"`
-		}
-		
-		decoder := json.NewDecoder(r.Body)
-		params := parameters{}
-		err := decoder.Decode(&params)
-		if err != nil {
-			log.Printf("Error decoding parameters %s", err)
-			w.WriteHeader(500)
-			return
-		}
-		if len(params.Body) > 140 {
-			respBody := errors{
-				Error: "Chirp is too long",
-			}
-			data, err := json.Marshal(respBody)
-			if err != nil {
-				log.Printf("Error marshalling JSON: %s", err)
-				w.WriteHeader(500)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(400)
-			w.Write(data)
-		} else {
-			respBody := valid{
-				Valid: true,
-			}
-			data, err := json.Marshal(respBody)
-			if err != nil {
-				log.Printf("Error marshalling JSON: %s", err)
-				w.WriteHeader(500)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(200)
-			w.Write(data)
-		}
-	})
-	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte("OK"))
-	})
-	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", apiCfg.fileServerHits.Load())))
-	})
-	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
-		apiCfg.fileServerHits.Store(0)
-		w.WriteHeader(200)
-	})
+	mux.HandleFunc("/api/validate_chirp", validate)
+	mux.HandleFunc("GET /api/healthz", health)
+	mux.HandleFunc("GET /admin/metrics", metrics)
+	mux.HandleFunc("POST /admin/reset", reset)
 	server := http.Server{
 		Addr: ":8080",
 		Handler: mux,
